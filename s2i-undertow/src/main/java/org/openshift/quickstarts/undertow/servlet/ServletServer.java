50d0dbe8ee223543c340daa7fed4b0ccc3e7e7ca
zapiz <ahlers_maren@web.de>
2023-12-13 12:47:26 +0100

    Nachricht geändert

--- s2i-undertow/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ s2i-undertow/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "und dann ist das Jahr auch schon vorbei")
+                                    .addInitParam("message", "und dann lalalala")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
